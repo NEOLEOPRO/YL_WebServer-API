d2cfeffada561f475652aa09189eb69eb19f043c
Леонид Мелентьев <lemelentev@miem.hse.ru>
2022-12-02 19:53:17 +0300

    V5

--- main.py
+++ main.py
@@ -385,5 +385,5 @@ def session_test():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    #app.run(port=8080, host='127.0.0.1')
+    #app.run(host='0.0.0.0', port=port)
+    app.run(port=8080, host='127.0.0.1')
